0283255bfc85df8addf0098b27db67c2f3cf384d
Антон <frostik96@gmail.com>
2025-09-30 18:15:21 +0300

    feat:kb_builder

---
+++ keyboard/reply_kb.py
@@ -0,0 +1,23 @@
+from aiogram.utils.keyboard import (
+    ReplyKeyboardBuilder,
+    KeyboardButton,
+    ReplyKeyboardMarkup
+)
+
+
+class ReplyBuilder:
+    """Динамическая клавиатура билдер."""
+    def __init__(self):
+        self.keyboard = ReplyKeyboardBuilder()
+
+    def add_buttons(self, buttons: list[str] | tuple[str]) -> None:
+        for button in buttons:
+            self.keyboard.add(KeyboardButton(text=button))
+
+
+    def get_keyboard(self,row: int = 1, is_one_time: bool = True) -> ReplyKeyboardMarkup:
+        return self.keyboard.adjust(row).as_markup(
+            resize_keyboard=True,
+            input_field_place_holder='меню',
+            one_time_keyboard=is_one_time
+        )
